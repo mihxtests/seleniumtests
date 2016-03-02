9c72eb8de9ac83a95f12a01ab1cc87847fc667e5
mihxtests <mihx@wp.pl>
2016-03-02 18:15:39 +0100

    Specflow tests

---
+++ SelTests/SomeTests/SampleSteps.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using Selenium.WebDriver.Extensions.JQuery;
+using TechTalk.SpecFlow;
+using By = Selenium.WebDriver.Extensions.JQuery.By;
+
+namespace SomeTests
+{
+    [Binding]
+    public class SomeSteps
+    {
+        public IWebDriver WebDriver
+        {
+            get { return (IWebDriver)ScenarioContext.Current["driver"]; }
+            set { ScenarioContext.Current.Add("driver", value); }
+        }
+
+        [Given(@"I have open ""(.*)""")]
+        public void GivenIHaveOpen(string webpage)
+        {
+            var webDriver = new ChromeDriver();
+            webDriver.Navigate().GoToUrl(webpage);
+
+            this.WebDriver = webDriver;
+        }
+
+        [Given(@"I have entered ""(.*)"" into search")]
+        public void GivenIHaveEnteredIntoSearch(string searchValue)
+        {
+            var webDriver = this.WebDriver;
+
+            var searchInput = webDriver.FindElement(By.JQuerySelector("#lst-ib"));
+            searchInput.SendKeys(searchValue);
+        }
+
+        [When(@"I press search")]
+        public void WhenIPressSearch()
+        {
+            var webDriver = this.WebDriver;
+
+            var button = webDriver.FindElement(By.JQuerySelector("button.lsb"));
+
+            button.Submit();
+        }
+
+        [Then(@"The first result should start with ""(.*)""")]
+        public void ThenTheFirstResultShouldStartWith(string result)
+        {
+            var webDriver = this.WebDriver;
+
+            var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(10));
+            var searchResult = wait.Until<IWebElement>(d => d.FindElements(By.JQuerySelector(".r a")).FirstOrDefault());
+            var foundResult = searchResult.Text;
+            webDriver.Close();
+
+            Assert.That(foundResult, Does.StartWith(result));
+        }
+    }
+
+}
---
+++ SelTests/SomeTests/sample.feature
@@ -0,0 +1,14 @@
+﻿Feature: sample
+	webpage
+
+@mytag
+Scenario Outline: SearchResult
+	Given I have open "http://google.com"
+	And I have entered <searchText> into search
+	When I press search
+	Then The first result should start with <resultText>
+	
+	Examples:
+	| searchText            | resultText                            |
+	| "dupa"                | "Dupa - Nonsensopedia"                |
+	| "kominek w domu plan" | "Projektowanie kominka krok po kroku" |
